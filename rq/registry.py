b0c535234832aaae2ed76c87d7829207528c59b1
Denis Guerra <denis.guerra@luizalabs.com>
2023-11-15 21:31:46 -0300

    Use old zadd if redis error

--- rq/registry.py
+++ rq/registry.py
@@ -1,3 +1,5 @@
+from redis.exceptions import RedisError
+
 from .compat import as_text
 from .connections import resolve_connection
 from .exceptions import NoSuchJobError
@@ -34,11 +36,17 @@ class BaseRegistry(object):
     def add(self, job, ttl=0, pipeline=None):
         """Adds a job to a registry with expiry time of now + ttl."""
         score = ttl if ttl < 0 else current_timestamp() + ttl
-        mapping = {job.id: score}
-        if pipeline is not None:
-            return pipeline.zadd(self.key, mapping)
+        try:
+            mapping = {job.id: score}
+            if pipeline is not None:
+                return pipeline.zadd(self.key, mapping)
 
-        return self.connection._zadd(self.key, mapping)
+            return self.connection._zadd(self.key, mapping)
+        except RedisError:
+            if pipeline is not None:
+                return pipeline.zadd(self.key, score, job.id)
+
+            return self.connection._zadd(self.key, score, job.id)
 
     def remove(self, job, pipeline=None):
         connection = pipeline if pipeline is not None else self.connection
